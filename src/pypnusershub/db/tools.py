e200f35059c44eaf39b6a4bf8f91527222c04643
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2017-03-08 09:49:41 +0100

    Revert "Revert "Working on usershub-auth-module genericity""

--- src/pypnusershub/db/tools.py
+++ src/pypnusershub/db/tools.py
@@ -6,9 +6,31 @@ from __future__ import (unicode_literals, print_function,
     DB tools not related to any model in particular.
 """
 
+from flask import current_app
+
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy import create_engine
 
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                          SignatureExpired, BadSignature)
+
+from pypnusershub.db import models
 from pypnusershub.utils import text_resource_stream
+
+class AccessRightsError(Exception):
+    pass
+
+
+class InsufficientRightsError(AccessRightsError):
+    pass
+
+
+class AccessRightsExpiredError(AccessRightsError):
+    pass
+
+
+class UnreadableAccessRightsError(AccessRightsError):
+    pass
 
 
 def init_schema(con_uri):
@@ -44,3 +66,32 @@ def load_fixtures(con_uri):
                 if line.strip():
                     engine.execute(line)
             engine.execute("COMMIT")
+
+
+def user_from_token(token, secret_key=None):
+    """Given a, authentification token, return the matching AppUser instance"""
+
+    secret_key = secret_key or current_app.config['SECRET_KEY']
+
+    try:
+        s = Serializer(current_app.config['SECRET_KEY'])
+        data = s.loads(token)
+
+        id_role = data['id_role']
+        id_app = data['id_application']
+        return (models.AppUser
+                      .query
+                      .filter(models.AppUser.id_role == id_role)
+                      .filter(models.AppUser.id_application == id_app)
+                      .one())
+
+    except NoResultFound:
+        return UnreadableAccessRightsError(
+            'No user withd id "{}" for app "{}"'.format(id_role, id_app)
+        )
+    except SignatureExpired:
+        raise AccessRightsExpiredError("Token expired")
+
+    except BadSignature:
+        return UnreadableAccessRightsError('Token BadSignature', 403)
+
