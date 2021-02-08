78e1aa8076452cd59d6f6b4cb34820a441546169
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-02-08 17:45:35 +0100

    fix t_roles.id_role sequence name

--- src/pypnusershub/db/models.py
+++ src/pypnusershub/db/models.py
@@ -67,7 +67,7 @@ class User(db.Model):
     __table_args__ = {'schema': 'utilisateurs'}
 
     TABLE_ID = Sequence(
-        't_roles_id_seq',
+        't_roles_id_role_seq',
         schema="utilisateurs",
     )
     groupe = db.Column(db.Boolean, default=False)
