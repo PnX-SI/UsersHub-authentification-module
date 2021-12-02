1a8d467d4969a53e16e436902e7c3a3551c14361
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-12-02 18:05:36 +0100

    fix keyError on confif[ID_APP]

--- src/pypnusershub/routes.py
+++ src/pypnusershub/routes.py
@@ -1,12 +1,11 @@
 # coding: utf8
 
-from __future__ import (unicode_literals, print_function,
-                        absolute_import, division)
+from __future__ import unicode_literals, print_function, absolute_import, division
 
 
-'''
+"""
 routes relatives aux application, utilisateurs et à l'authentification
-'''
+"""
 
 import json
 import logging
@@ -14,11 +13,21 @@ import logging
 import datetime
 from functools import wraps
 
-from flask import Blueprint, escape, request, Response, current_app, redirect, g, jsonify, session
+from flask import (
+    Blueprint,
+    escape,
+    request,
+    Response,
+    current_app,
+    redirect,
+    g,
+    jsonify,
+    session,
+)
 
 from sqlalchemy.orm import exc
 import sqlalchemy as sa
-
+from werkzeug.exceptions import BadRequest
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 from pypnusershub.db import models, db
@@ -58,31 +67,32 @@ log = logging.getLogger(__name__)
 
 
 class ConfigurableBlueprint(Blueprint):
-
     def register(self, app, *args, **kwargs):
 
         # set cookie autorenew
-        expiration = app.config.get('COOKIE_EXPIRATION', 3600)
-        cookie_autorenew = app.config.get('COOKIE_AUTORENEW', True)
-        app.config['PASS_METHOD'] = app.config.get('PASS_METHOD', 'hash')
+        expiration = app.config.get("COOKIE_EXPIRATION", 3600)
+        cookie_autorenew = app.config.get("COOKIE_AUTORENEW", True)
+        app.config["PASS_METHOD"] = app.config.get("PASS_METHOD", "hash")
 
         if cookie_autorenew:
 
             @app.after_request
             def after_request(response):
                 try:
-                    set_cookie = response.headers.get('Set-Cookie', '')
-                    is_setting_token = set_cookie.startswith('token=')
-                    is_token_set = request.cookies.get('token')
+                    set_cookie = response.headers.get("Set-Cookie", "")
+                    is_setting_token = set_cookie.startswith("token=")
+                    is_token_set = request.cookies.get("token")
                     if is_token_set and not is_setting_token:
                         cookie_exp = datetime.datetime.utcnow()
                         cookie_exp += datetime.timedelta(seconds=expiration)
-                        response.set_cookie('token',
-                                            request.cookies['token'],
-                                            expires=cookie_exp)
-                        response.set_cookie('currentUser',
-                                            request.cookies['currentUser'],
-                                            expires=cookie_exp)
+                        response.set_cookie(
+                            "token", request.cookies["token"], expires=cookie_exp
+                        )
+                        response.set_cookie(
+                            "currentUser",
+                            request.cookies["currentUser"],
+                            expires=cookie_exp,
+                        )
                     return response
                 # TODO: replace the generic exception by a specific one
                 except Exception:
@@ -92,7 +102,7 @@ class ConfigurableBlueprint(Blueprint):
         parent.register(app, *args, **kwargs)
 
 
-routes = ConfigurableBlueprint('auth', __name__)
+routes = ConfigurableBlueprint("auth", __name__)
 
 
 def check_auth(
@@ -100,24 +110,24 @@ def check_auth(
     get_role=False,
     redirect_on_expiration=None,
     redirect_on_invalid_token=None,
-    redirect_on_insufficient_right=None
+    redirect_on_insufficient_right=None,
 ):
     def _check_auth(fn):
         @wraps(fn)
         def __check_auth(*args, **kwargs):
             try:
                 # TODO: better name and configurability for the token
-                user = user_from_token(request.cookies['token'])
+                user = user_from_token(request.cookies["token"])
 
                 if user.id_droit_max < level:
                     # HACK better name for callback if right are low
                     if redirect_on_insufficient_right:
-                        log.info('Privilege too low')
+                        log.info("Privilege too low")
                         return redirect(redirect_on_insufficient_right, code=302)
-                    return Response('Forbidden', 403)
+                    return Response("Forbidden", 403)
 
                 if get_role:
-                    kwargs['id_role'] = user.id_role
+                    kwargs["id_role"] = user.id_role
 
                 g.user = user
 
@@ -127,134 +137,125 @@ def check_auth(
                 if redirect_on_expiration:
                     res = redirect(redirect_on_expiration, code=302)
                 else:
-                    res = Response('Token Expired', 403)
-                res.set_cookie('token', '', expires=0)
+                    res = Response("Token Expired", 403)
+                res.set_cookie("token", "", expires=0)
                 return res
 
             except KeyError as e:
-                if 'token' not in e.args:
+                if "token" not in e.args:
                     raise
                 if redirect_on_expiration:
                     return redirect(redirect_on_expiration, code=302)
-                return Response('No token', 403)
+                return Response("No token", 403)
 
             except UnreadableAccessRightsError:
-                log.info('Invalid Token : BadSignature')
+                log.info("Invalid Token : BadSignature")
                 # invalid token
                 if redirect_on_invalid_token:
                     res = redirect(redirect_on_invalid_token, code=302)
                 else:
                     res = Response(
-                        'Token BadSignature or token not coresponding to the app', 403)
-                res.set_cookie('token', '', expires=0)
+                        "Token BadSignature or token not coresponding to the app", 403
+                    )
+                res.set_cookie("token", "", expires=0)
                 return res
 
             except Exception as e:
                 trap_all_exceptions = current_app.config.get(
-                    'TRAP_ALL_EXCEPTIONS',
-                    True
+                    "TRAP_ALL_EXCEPTIONS", True
                 )
                 if not trap_all_exceptions:
                     raise
                 log.critical(e)
-                msg = json.dumps({'type': 'Exception', 'msg': repr(e)})
+                msg = json.dumps({"type": "Exception", "msg": repr(e)})
                 return Response(msg, 403)
 
         return __check_auth
+
     return _check_auth
 
 
-@routes.route('/login', methods=['POST'])
+@routes.route("/login", methods=["POST"])
 def login():
     try:
         user_data = request.json
         try:
-            id_app = user_data.get('id_application', current_app.config['ID_APP'])
-            login = user_data['login']
+            id_app = user_data.get("id_application", current_app.config.get("ID_APP"))
+            login = user_data.get("login")
+            password = user_data.get("password")
+            if id_app is None or login is None or password is None:
+                msg = json.dumps(
+                    "One of the following paramter is required ['id_application', 'login', 'password']"
+                )
+                return Response(msg, status=400)
 
-            user = (models.AppUser
-                    .query
-                    .filter(models.AppUser.identifiant == login)
-                    .filter(models.AppUser.id_application == id_app)
-                    .one())
+            user = (
+                models.AppUser.query.filter(models.AppUser.identifiant == login)
+                .filter(models.AppUser.id_application == id_app)
+                .one()
+            )
 
             # Return child application
-            sub_app = models.AppUser.query.join(
-                models.Application, models.Application.id_application == models.AppUser.id_application
-            ).filter(
-                models.Application.id_parent == id_app
-            ).filter(
-                models.AppUser.id_role == user.id_role
-            ).all()
+            sub_app = (
+                models.AppUser.query.join(
+                    models.Application,
+                    models.Application.id_application == models.AppUser.id_application,
+                )
+                .filter(models.Application.id_parent == id_app)
+                .filter(models.AppUser.id_role == user.id_role)
+                .all()
+            )
 
             user_dict = user.as_dict()
-            user_dict['apps'] = {
-                s.id_application: s.id_droit_max for s in sub_app}
-
-        except KeyError as e:
-            parameters = ", ".join(e.args)
-            msg = json.dumps({
-                'type': 'login',
-                'msg': 'The following parameters are required: %s' % parameters
-            })
-            # Initially the status code used was 490, so it's kept as a
-            # default value to maintain compat. However, 400 is the
-            # appropriate code and you can choose to set it
-            # up that way with the BAD_LOGIN_STATUS_CODE setting.
-            status_code = current_app.config.get('BAD_LOGIN_STATUS_CODE', 490)
-            return Response(msg, status=status_code)
-
+            user_dict["apps"] = {s.id_application: s.id_droit_max for s in sub_app}
         except (exc.NoResultFound, AssertionError) as e:
 
-            msg = json.dumps({
-                'type': 'login',
-                'msg': (
-                    'No user found with the username "{login}" for '
-                    'the application with id "{id_app}"'
-                ).format(login=escape(login), id_app=id_app)
-            })
+            msg = json.dumps(
+                {
+                    "type": "login",
+                    "msg": (
+                        'No user found with the username "{login}" for '
+                        'the application with id "{id_app}"'
+                    ).format(login=escape(login), id_app=id_app),
+                }
+            )
             log.info(msg)
-            status_code = current_app.config.get('BAD_LOGIN_STATUS_CODE', 490)
+            status_code = current_app.config.get("BAD_LOGIN_STATUS_CODE", 490)
             return Response(msg, status=status_code)
 
         except Exception as e:
             log.critical(e)
-            msg = json.dumps({
-                'type': 'bug',
-                'msg': 'Unkown error during login'
-            })
+            msg = json.dumps({"type": "bug", "msg": "Unkown error during login"})
             log.info(msg)
             return Response(msg, status=500)
 
-        if not user.check_password(user_data['password']):
-            msg = json.dumps({
-                'type': 'password',
-                'msg': 'Mot de passe invalide'
-            })
+        if not user.check_password(user_data["password"]):
+            msg = json.dumps({"type": "password", "msg": "Mot de passe invalide"})
             log.info(msg)
-            status_code = current_app.config.get('BAD_LOGIN_STATUS_CODE', 490)
+            status_code = current_app.config.get("BAD_LOGIN_STATUS_CODE", 490)
             return Response(msg, status=status_code)
 
         # Génération d'un token
         token = user_to_token(user)
         cookie_exp = datetime.datetime.utcnow()
-        cookie_exp += datetime.timedelta(seconds=current_app.config['COOKIE_EXPIRATION'])
-        resp = Response(json.dumps({'user': user_dict,
-                                    'expires': str(cookie_exp)}))
-        resp.set_cookie('token', token, expires=cookie_exp)
+        cookie_exp += datetime.timedelta(
+            seconds=current_app.config["COOKIE_EXPIRATION"]
+        )
+        resp = Response(json.dumps({"user": user_dict, "expires": str(cookie_exp)}))
+        resp.set_cookie("token", token, expires=cookie_exp)
 
         return resp
     except Exception as e:
-        msg = json.dumps({'login': False, 'msg': repr(e)})
+        msg = json.dumps({"login": False, "msg": repr(e)})
         return Response(msg, status=403)
 
 
-@routes.route('/logout', methods=['GET', 'POST'])
+@routes.route("/logout", methods=["GET", "POST"])
 def logout():
     params = request.args
-    if 'redirect' in params:
-        resp = redirect(params['redirect'], code=302)
+    if "redirect" in params:
+        resp = redirect(params["redirect"], code=302)
     else:
         resp = redirect("", code=302)
-    resp.delete_cookie('token')
+    resp.delete_cookie("token")
     return resp
