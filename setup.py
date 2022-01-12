54140e6ce304272e68a4e20fa35464e3c41af2d4
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-01-12 13:21:23 +0100

    fix setup.py classifiers

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setuptools.setup(
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
-                 'License :: OSI Approved :: GNU Affero General Public License v3'
+                 'License :: OSI Approved :: GNU Affero General Public License v3',
                  'Operating System :: OS Independent'],
 )
