f820604014a1d63f6a2e1808a0da518bbd1b834d
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-12-14 10:33:32 +0100

    feat(api): add Organism schema and add it to User

--- src/pypnusershub/schemas.py
+++ src/pypnusershub/schemas.py
@@ -6,6 +6,13 @@ from pypnusershub.env import ma, db
 from pypnusershub.db.models import User, Organisme
 
 
+class OrganismeSchema(SmartRelationshipsMixin, ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Organisme
+        load_instance = True
+        sqla_session = db.session
+
+
 class UserSchema(SmartRelationshipsMixin, ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
@@ -17,6 +24,7 @@ class UserSchema(SmartRelationshipsMixin, ma.SQLAlchemyAutoSchema):
     max_level_profil = fields.Integer()
     nom_complet = fields.String()
     groups = fields.Nested(lambda: UserSchema, many=True)
+    organisme = fields.Nested(OrganismeSchema)
 
     # TODO: remove this and fix usage of the schema
     @pre_load
@@ -24,10 +32,3 @@ class UserSchema(SmartRelationshipsMixin, ma.SQLAlchemyAutoSchema):
         if isinstance(data, int):
             return dict({"id_role": data})
         return data
-
-
-class OrganismeSchema(SmartRelationshipsMixin, ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Organisme
-        load_instance = True
-        sqla_session = db.session
