9f127f892ac8ac201de80cf500a69c0f6f882d76
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-07-05 09:55:34 +0200

    add missing auth manager in the conftest

--- src/pypnusershub/tests/conftest.py
+++ src/pypnusershub/tests/conftest.py
@@ -6,6 +6,7 @@ from utils_flask_sqla.tests.utils import JSONClient
 
 from pypnusershub.env import db, ma
 from pypnusershub.login_manager import login_manager
+from pypnusershub.auth.auth_manager import auth_manager
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -13,15 +14,16 @@ def app():
     app = Flask("pypnusershub")
     from pypnusershub.routes import routes
 
-    app.register_blueprint(routes)
     app.testing = True
     app.test_client_class = JSONClient
-
+    app.config["AUTHENTICATION"] = {}
     app.config.from_envvar("USERSHUB_AUTH_MODULE_SETTINGS")
     app.testing = True
     db.init_app(app)
     ma.init_app(app)
+    auth_manager.init_app(app)
     login_manager.init_app(app)
+
     with app.app_context():
         transaction = db.session.begin_nested()  # execute tests in a savepoint
         yield app
