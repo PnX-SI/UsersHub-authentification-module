17f5e484baac19616ff76e9803ed418ba311a56e
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-06-03 20:13:29 +0200

    feat : add api_key to t_roles

---
+++ src/pypnusershub/migrations/versions/17122ba73a6f_add_apikey_to_t_roles.py
@@ -0,0 +1,29 @@
+"""add apikey to t_roles
+
+Revision ID: 17122ba73a6f
+Revises: b3dec57f13d8
+Create Date: 2025-06-03 20:01:52.252581
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "17122ba73a6f"
+down_revision = "b3dec57f13d8"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        column=sa.Column("api_key", sa.UnicodeText(), server_default=None),
+        table_name="t_roles",
+        schema="utilisateurs",
+    )
+
+
+def downgrade():
+    op.drop_column(column_name="api_key", table_name="t_roles", schema="utilisateurs")
