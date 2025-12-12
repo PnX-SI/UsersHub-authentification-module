1bbfe62dc2741b6152cae2891492cec521532eb7
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-12-12 17:16:56 +0100

    remove old method

--- src/pypnusershub/routes.py
+++ src/pypnusershub/routes.py
@@ -221,26 +221,3 @@ def authorize(provider="local_provider"):
 
     # if auth_provider.is_external:
     return redirect(current_app.config["URL_APPLICATION"])
-
-
-def insert_or_update_organism(organism) -> dict:
-    """
-    Insert an organism.
-
-    The organism may eventually be updated rather than inserted.
-
-    Parameters
-    ----------
-    organism : dict
-        A dictionary containing the organism data.
-
-    Returns
-    -------
-    dict
-        The inserted or updated organism.
-    """
-    organism_schema = OrganismeSchema()
-    organism = organism_schema.load(organism)
-    db.session.add(organism)
-    db.session.commit()
-    return organism_schema.dump(organism)
