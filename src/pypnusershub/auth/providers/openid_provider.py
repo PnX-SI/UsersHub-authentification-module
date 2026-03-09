4946963ccc2d3be650fe78d850309621ec03c866
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2026-03-09 17:39:18 +0100

    fix: add code mutualization

--- src/pypnusershub/auth/providers/openid_provider.py
+++ src/pypnusershub/auth/providers/openid_provider.py
@@ -72,25 +72,17 @@ class OpenIDProvider(Authentication):
             else []
         )
 
-        # Auto-validation: create/update user and allow login immediately
-        if self.auto_validate_new_user:
-            user = self.insert_or_update_role(
-                new_user,
-                source_groups=source_groups,
-                reconciliate_attr=self.reconciliate_attr,
-            )
-            db.session.commit()
-            return user
-
-        # Manual validation: existing users can still log in
-        user = db.session.execute(
+        # Existing user
+        existing_user = db.session.execute(
             sa.select(models.User).where(
                 getattr(models.User, self.reconciliate_attr)
                 == new_user[self.reconciliate_attr]
             )
         ).scalar_one_or_none()
 
-        if user:
+        # Manual validation: existing users can still log in
+        # Auto-validation: create/update user and allow login immediately
+        if existing_user or self.auto_validate_new_user:
             user = self.insert_or_update_role(
                 new_user,
                 source_groups=source_groups,
